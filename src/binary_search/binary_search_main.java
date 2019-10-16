f40fbfda33b9caac2bc4caa4afd8bb3c7727cb9a
Jeschusmorio <mpetrovic1@tsn.at>
2019-10-16 18:34:18 +0200

    modified to the solution of the professor

--- src/binary_search/binary_search_main.java
+++ src/binary_search/binary_search_main.java
@@ -4,75 +4,37 @@ import java.util.Random;
 public class binary_search_main {
 
 	public static void main(String[] args) {
-		Random r = new Random();
-		//int laenge = r.nextInt(100)+1;
-		int laenge=100;
-		int[] a = new int[laenge];
-		int zahl = r.nextInt(100);
-		//int zahl = 49;
-		for (int i = 0; i < a.length; i++) {
-			a[i] = r.nextInt(100);
-			//a[i] = i;
-			//System.out.print(a[i]+", ");
+		for (int i = 0; i < menge.length; i++) {
+			menge[i] = i;
 		}
-		//System.out.println();
-		if (sequenzSearch(a, zahl)) {
-			System.out.println(zahl+" ist in diesem Array enthalten!");
-		}
-		else {
-			System.out.println(zahl+" ist nicht in diesem Array enthalten!");
-		}
-		System.out.println();
-		if (binarySearch(a, zahl)) {
-			System.out.println(zahl+" ist in diesem Array enthalten!");
-		}
-		else {
-			System.out.println(zahl+" ist nicht in diesem Array enthalten!");
+		for (int i = 0; i < menge.length; i++) {
+			suche(i);
 		}
 	}
-	public static boolean sequenzSearch(int[] a, int zahl) {
-		System.out.println("Sequenzielle Suche:");
-		boolean enthalten = false;
+	
+	static int[] menge = new int[100];
+	
+	public static void suche (int suchzahl) {
+		System.out.println("suche "+suchzahl+" ---------------");
+		int anfang = 0;
+		int ende = menge.length - 1;
 		int zaehler = 0;
-		for (int i = 0; i < a.length; i++) {
+		
+		while (anfang <= ende) {
 			zaehler++;
-			if (zahl == a[i]) {
-				enthalten = true;
+			int halbe = anfang + (int)((ende - anfang) / 2);
+			System.out.println("suchschritt "+anfang+" "+ende+" "+halbe);
+			if (menge[halbe] == suchzahl) {
+				System.out.println("found");
 				break;
 			}
-		}
-		System.out.println("Suchschritte: "+zaehler);
-		return enthalten;
-	}
-	public static boolean binarySearch(int[] a, int zahl) {
-		System.out.println("Binaere Suche:");
-		boolean enthalten = false;
-		Arrays.sort(a);
-		//System.out.println(a.length);
-		/*for (int i = 0; i < a.length; i++) { 
-			System.out.print(a[i]+", ");
-		}
-		System.out.println();*/
-		int zaehler = 0;
-		int max_index = a.length-1;
-		int min_index = 0;
-		int index;
-		while (min_index <= max_index) { //zaehler < Math.log(a.length)
-			zaehler++;
-			index = (max_index + min_index) / 2;
-			//System.out.println(index);
-			if (zahl == a[index]) {
-				enthalten = true;
-				break;
+			else if (suchzahl < menge[halbe]) {
+				ende = halbe - 1;
 			}
-			if (zahl < a[index]) {
-				max_index = index-1; //Der Wert ist nicht auf der Stelle "index" somit wird die Suchrange verringert, verhindert ausserdem endlosen Schleifendurchlauf
-			}
-			if (zahl > a[index]) {
-				min_index = index+1;
+			else {
+				anfang = halbe + 1;
 			}
 		}
-		System.out.println("Suchschritte: "+zaehler);
-		return enthalten;
+		System.out.println(zaehler);
 	}
 }
